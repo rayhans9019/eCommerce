2ac33e47e7a41d92e99add18dc061ebcbbb6034a
rayhans9019 <63007106+rayhans9019@users.noreply.github.com>
2020-05-12 15:52:41 +0600

    Contact Page Done

--- public/contact.php
+++ public/contact.php
@@ -18,11 +18,7 @@
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <h2 class="section-heading">Keep in touch</h2>
-                    <h3 class="section-subheading">
-                        <?php
-                            display_message();
-                        ?>
-                    </h3>
+                    <h3 class="section-subheading text-muted"></h3>
                 </div>
             </div>
             <div class="row">
